7234ee17d850315e4c83e8516e7354e6703ddcb4
MauBouabci <maubouabci@gmail.com>
2014-07-27 01:53:37 +0100

    Item class included

--- knapsack.py
+++ knapsack.py
@@ -13,7 +13,7 @@ class Item(object):
     def getValue(self):
         return self.value
 
-    def getWeoght(self):
+    def getWeight(self):
         return self.weight
 
     def __str__(self):
@@ -22,15 +22,33 @@ class Item(object):
         return result
 
 
+def buildItems():
+    names = ['clock', 'painting', 'radio', 'vase', 'book', 'computer']
+    values = [3,2,4,4]
+    weights = [4,3,2,3]
+    Items = []
+    for i in range(len(values)):
+        Items.append(Item(names[i], values[i], weights[i]))
+    return Items
+
+
 def knapsack(items, capacity):
     import numpy as np
 
-    A = np.zeros(items, capacity)
+    numberItems = len(items)
+    A = np.zeros((numberItems+1, capacity+1))
 
-    for i in range(capacity):
-        for x in range(items):
-            A[i, x] = max(A[i - 1, x], A[i - 1, x - items.w[i]] + items.w[x])
+    for i in range(1, numberItems+1):
+        for x in range(capacity+1):
+            if x - items[i].getWeight() >= 0:
+                secondTerm = A[i - 1, x - items[i].getWeight()] + items[i].getValue()
+            else:
+                secondTerm = 0
+            A[i, x] = max(A[i - 1, x], secondTerm)
 
-    return A[x, w]
+    return A[i, x]
 
+items = buildItems()
+a=knapsack(items, 6)
+a = 1
 
